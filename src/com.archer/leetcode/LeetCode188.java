c06616240fe0b3a270290f604f43396aa098b9d3
archeryue <archeryue7@gmail.com>
2020-05-13 15:35:30 +0800

    ADD: another code for LeetCode 188

--- src/com.archer/leetcode/LeetCode188.java
+++ src/com.archer/leetcode/LeetCode188.java
@@ -6,7 +6,7 @@ package com.archer.leetcode;
  */
 public class LeetCode188 {
 
-    private static int maxProfit(int k, int[] prices) {
+        private static int maxProfitA(int k, int[] prices) {
         if (prices.length == 0 || prices.length == 1) {
             return 0;
         }
@@ -39,9 +39,29 @@ public class LeetCode188 {
         return profit;
     }
 
+    private static int maxProfitB(int k, int[] prices) {
+        if (prices == null || prices.length < 2 || k < 1) {
+            return 0;
+        }
+        k = Math.min(k, prices.length / 2);
+        int[] local = new int[k + 1];
+        int[] global = new int[k + 1];
+        for (int i = 1; i < prices.length; i++) {
+            int diff = prices[i] - prices[i - 1];
+            int[] tmp = new int[k + 1];
+            for (int j = 1; j <= k; j++) {
+                local[j] = Math.max(global[j - 1], local[j]) + diff;
+                tmp[j] = Math.max(global[j], local[j]);
+            }
+            global = tmp;
+        }
+        return global[k];
+    }
+
     public static void main(String[] args) {
-        int[] prices = new int[] {4,4,6,1,1,4,2,5};
-        System.out.println(maxProfit(1, prices));
+        int[] prices = new int[] {2, 4, 1};
+        System.out.println(maxProfitA(2, prices));
+        System.out.println(maxProfitB(2, prices));
     }
 
 }
